466648ec77028f01ec79d53a06ba11e187f66e33
Simon Lee <simonlee0306@gmail.com>
2018-10-23 19:02:07 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,75 @@
 # Write your code here!
+require "pry"
+def game_hash
+  game = {
+    :home => { 
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+        "Alan Anderson" => {
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>           
+        }
 
+        
+      }
+    }
+    :away => { 
+      :team_name => "",
+      :colors => [],
+      :players => {}
+    }
+  }
 
+end
 
+def num_points_scored
 
+end
 
+def shoe_size
 
+end
 
+def team_colors
 
+end
 
+def team_names
 
+end
+
+def player_numbers
+
+end
+
+def player_stats
+
+end
+
+def big_shoe_rebounds
+
+end
+
+#### BONUS ####
+def most_points_scored
+
+end
+
+def winning_team
+
+end
+
+def player_with_longest_name
+
+end
+
+def long_name_steals_a_ton
+
+end
